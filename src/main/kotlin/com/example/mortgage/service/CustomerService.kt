c3a2bd69d77e338b0b47d1fb4c1a906e0ea1049d
Ted Erickson <c-theodore.erickson@charter.com>
2021-02-04 08:30:05 -0700

    parameter name change

--- src/main/kotlin/com/example/mortgage/service/CustomerService.kt
+++ src/main/kotlin/com/example/mortgage/service/CustomerService.kt
@@ -1,7 +1,6 @@
 package com.example.mortgage.service
 
 import com.example.mortgage.digest.CustomerDigest
-import com.example.mortgage.model.Customer
 import java.util.*
 
 interface CustomerService {
@@ -10,5 +9,5 @@ interface CustomerService {
     fun deleteCustomer(id: Long)
     fun getCustomerById(id: Long): Optional<CustomerDigest>
     fun getCustomers(): MutableList<CustomerDigest>
-    fun updateCustomer(customer: CustomerDigest): CustomerDigest?
+    fun updateCustomer(customerDigest: CustomerDigest): CustomerDigest?
 }
